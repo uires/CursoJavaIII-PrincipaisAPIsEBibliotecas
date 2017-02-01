ac8c74187a0f120d32e9be2130b85f5c714f60df
uires deivide <uiresdeivide1@gmail.com>
2017-02-01 14:41:22 -0300

    Testando classe

---
+++ src/Conta.java
@@ -0,0 +1,57 @@
+public class Conta {
+	private int agencia;
+	private String nome;
+	private double saldo;
+	private String cpf;
+
+	// Getters e Setters: 
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/PrincipalTeste.java
@@ -0,0 +1,11 @@
+public class PrincipalTeste {
+
+	public static void main(String[] args) {
+		Conta c = new Conta();
+		
+		
+		c.setNome("Jessu ll");
+		System.out.println(c.getNome());
+	}
+
+}
