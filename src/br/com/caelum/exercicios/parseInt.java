0890e0718a5c990043bfbe3c3950283914a2d184
uires deivide <uiresdeivide1@gmail.com>
2017-02-08 15:33:17 -0300

    aula 3 -COnversão de String para INT

---
+++ src/br/com/caelum/exercicios/parseInt.java
@@ -0,0 +1,23 @@
+package br.com.caelum.exercicios;
+
+public class parseInt {
+
+	public static void main(String[] args) {
+		String numero = "762";
+		System.out.println("Imprimindo a string: " + numero);
+
+		int resultado = converteParaInt(numero);
+		System.out.println("Imprimindo o int: " + resultado);
+	}
+
+	private static int converteParaInt(String numero) {
+		int resultado = 0;
+		while (numero.length() > 0) {
+			char algarismo = numero.charAt(0);
+			resultado = resultado * 10 + (algarismo - '0');
+			numero = numero.substring(1);
+		}
+		return resultado;
+	}
+
+}
