45f940ecc70d6460a78ed6d876f8e01a702526d4
uires deivide <uiresdeivide1@gmail.com>
2017-02-08 20:01:57 -0300

    Aula 4- Exercício final, utilizando Scanner e PrintStream

--- src/br/com/caelum/exercicios/testeIO.java
+++ src/br/com/caelum/exercicios/testeIO.java
@@ -1,39 +1,22 @@
 package br.com.caelum.exercicios;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.Scanner;
+
 
 
 public class testeIO {
 	public static void main(String[] args) throws IOException {
+		
+		Scanner entrada = new Scanner(System.in);
+		PrintStream saida = new PrintStream(new FileOutputStream("arquivo.txt"));
+		
+		while(entrada.hasNextLine()){			
+			saida.println(entrada.nextLine());			
+		}
+		
+		saida.close();
+		entrada.close();
 
-		InputStream is = System.in;
-		InputStreamReader isr = new InputStreamReader(is);
-		BufferedReader br = new BufferedReader(isr);
-		
-		OutputStream os = new FileOutputStream("saida.txt");	
-        OutputStreamWriter osw = new OutputStreamWriter(os);
-        BufferedWriter bw = new BufferedWriter(osw);
-		
-		String linha = br.readLine();
-		
-		while (linha!= null) {
-			bw.append(linha);	  
-	    	bw.newLine(); 
-			linha =br.readLine();
-		}			
-		br.close();
-		bw.close();
 	}
-			
-	
 }
-
