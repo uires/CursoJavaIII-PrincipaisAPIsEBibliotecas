0a0a81438091ef91cf1252201110d21a24fc6e90
uires deivide <uiresdeivide1@gmail.com>
2017-02-08 22:32:08 -0300

    Aula 5- Criando classe, usando Collections.sort, ArrayList para ordenar e importando bibliotecas

--- src/br/com/caelum/testeprincipais/testeElementos.java
+++ src/br/com/caelum/testeprincipais/testeElementos.java
@@ -1,15 +1,27 @@
 package br.com.caelum.testeprincipais;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 //; Aula 5 -ArrayList, Collections Framework
 
 public class testeElementos {
 	public static void main(String[] args) {
-		ArrayList nomes = new ArrayList();
-		nomes.add("LOle");
-		nomes.add("Russians");
+		ArrayList<String> nomes = new ArrayList<String>();
+		
+		
+		nomes.add("Uires");
+		nomes.add("Bela");
+		nomes.add("Laisa");
+		nomes.add("Nilzete");
+
+		Collections.sort(nomes);
+		
+		
+		System.out.println(nomes.get(0));
 		System.out.println(nomes.get(1));
-		System.out.println(nomes.contains("LOle"));
+		System.out.println(nomes.get(2));
+		System.out.println(nomes.get(3));
+		
 	}
 }
