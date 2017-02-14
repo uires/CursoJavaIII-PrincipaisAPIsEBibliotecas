e0d0f8b257c00f39717dcff1f337fd795b3fcb3b
uires deivide <uiresdeivide1@gmail.com>
2017-02-14 18:46:01 -0300

    Aula 5- ArrayList, Reverse Collections, TreeSet

---
+++ src/br/com/caelum/testeprincipais/exercicio.java
@@ -0,0 +1,23 @@
+package br.com.caelum.testeprincipais;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeSet;
+
+
+
+public class exercicio {
+	public static void main(String[] args) {
+		List<Integer> conjunto = new ArrayList<>();
+			for (int i = 1000; i >= 1; i--) {
+			conjunto.add(i);
+		}
+			Collections.reverse(conjunto);	
+		for (Integer i : conjunto) {
+			System.out.print(i + " ");
+		}
+		
+	}
+}
