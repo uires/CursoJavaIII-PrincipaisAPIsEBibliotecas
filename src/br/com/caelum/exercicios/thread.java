594de506a35eda638b4a687f7845f96b96d743a4
uires deivide <uiresdeivide1@gmail.com>
2017-02-15 22:12:05 -0300

    AUla 6- Testando alterações

--- src/br/com/caelum/exercicios/thread.java
+++ src/br/com/caelum/exercicios/thread.java
@@ -3,8 +3,8 @@ package br.com.caelum.exercicios;
 public class thread implements Runnable {
 	
 	public void run(){
-		for(int i = 0;i <= 10000;i++){
-			System.out.println(i);
+		for(int i = 0;i <= 1000000;i++){
+			System.out.println(i * 2);
 		}
 		
 	}
--- src/br/com/caelum/exercicios/thread2.java
+++ src/br/com/caelum/exercicios/thread2.java
@@ -4,7 +4,7 @@ public class thread2 implements Runnable {
 		
 		public void run(){
 			for(int i = 10000;i >= 1;i--){
-				System.out.println(i);
+				System.out.println(i - 1000);
 			}
 		}
 	}
